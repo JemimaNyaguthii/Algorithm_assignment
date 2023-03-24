0efecaa72af8414921cf6f84797cd97492b4a4f2
JemimaNyaguthii <jemimanyaguthiiwambugu@gmail.com>
2023-03-24 16:52:44 +0300

    commits

---
+++ binary.js
@@ -0,0 +1,12 @@
+function merge_sort(unsorted_Array){
+    const middle_index=unsorted_Array.lenght/2
+    if(unsorted_Array.length<2){
+        return unsorted_Array
+    }
+console.log(unsorted_Array)
+ 
+     }
+    
+let num=[45,12,6,89,2,5]
+let target=6;
+console.log(binary(num,target))
