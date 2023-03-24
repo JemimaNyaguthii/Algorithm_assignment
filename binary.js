809ad20716081c9bd1dc8f0fc930f1e34b698729
JemimaNyaguthii <jemimanyaguthiiwambugu@gmail.com>
2023-03-24 20:18:15 +0300

    commits

--- binary.js
+++ binary.js
@@ -1,3 +1,7 @@
+
+// Given an array of unsorted numbers, return the index of the following target if the target exists in the array. If the target is not found, return null
+// let num = [45,12,6,89,2,5]
+// let target = 6
 function merge_sort(unsorted_Array){
     const middle_index=unsorted_Array.lenght/2
     if(unsorted_Array.length<2){
@@ -9,4 +13,20 @@ console.log(unsorted_Array)
     
 let num=[45,12,6,89,2,5]
 let target=6;
-console.log(binary(num,target))
+console.log((num,target))
+// Given the following array, search for the following target
+// let target = 34
+// let arr2 = [1,4,78,2,67,3];
+function merge_sort(unsorted_Array){
+    const middle_index=unsorted_Array.lenght/2
+    if(unsorted_Array.length<2){
+        return unsorted_Array
+    }
+console.log(unsorted_Array)
+ 
+     }
+    
+let num2=[1,4,78,2,67,3]
+let target2=34;
+console.log((num2,target2))
+
